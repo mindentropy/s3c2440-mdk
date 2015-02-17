070fdab39ce016ce9de68148ae42920437c42551
mindentropy <mindentropy@gmail.com>
2015-02-17 22:26:54 +0530

    Stack setup and task list updation. Setup stack for different operation modes. Updated task list

--- src/os/os_glue.s
+++ src/os/os_glue.s
@@ -19,6 +19,17 @@
  */
 
 .section .text
+
+.equ ABORT_MODE, 	 0x17
+.equ FIQ_MODE,		 0x11
+.equ IRQ_MODE,		 0x12
+.equ SVC_MODE,		 0x13
+.equ SYS_MODE,		 0x1F
+.equ UNDEF_MODE,	 0x1B
+.equ USR_MODE,		 0x10
+.equ CPSR_MODE_MASK, 0x1F
+.equ NoInt, 		 0xC0
+
 .code 32
 .globl start_os
 
@@ -59,18 +70,72 @@ clear_bss:
 	str r3,[r1],#4
 	bne clear_bss
 
+stack_setup:
+	/* We are in the supervisor (svc) mode. Setup up the stack */
 	/* load r13 i.e. svc stack pointer with svc_stack_pointer */
 	ldr r13,svc_stack_pointer
 
+	/*** Setup stack for irq mode ***/
+	/* See example in s3c2440 Pg 3-20 for setup */
+
+	mrs r0,cpsr 				/* Take a copy of the register */
+	bic r0,r0,#CPSR_MODE_MASK 	/* Clear the mode bits */
+	orr r0,r0,#IRQ_MODE 		/* Set the mode to irq */
+	orr r0,r0,#NoInt 			/* Disable irq,fiq interrupt */
+	msr cpsr,r0     			/* Write the modified value back to CPSR */
+	ldr r13,irq_stack_pointer 	/* Load the stack with the irq stack */
+
+	/*** Setup stack for fiq mode ***/
+	mrs r0,cpsr 				/* Take a copy of the register */
+	bic r0,r0,#CPSR_MODE_MASK 	/* Clear the mode bits */
+	orr r0,r0,#FIQ_MODE         /* Setup fiq_mode */
+	orr r0,r0,#NoInt 			/* Disable irq,fiq interrupt */
+	msr cpsr,r0
+	
+	/* 
+	 * r8-r14 are shadow registers for fiq. Here r8-r12 are general purpose.
+	 * Hence setting it to 0
+	 */
+
+	ldr r8,=0
+	ldr r9,=0
+	ldr r10,=0
+	ldr r11,=0
+	ldr r12,=0
+	ldr r13,fiq_stack_pointer
+
+	/*** Setup stack for usr and sys mode ***/
+	/* Usr and system mode share the same stack */
+	/* 
+	 * I cannot be in the user mode as there is no way to change the cpsr
+	 * in user mode. Hence the stack has to be changed in system mode as the
+	 * stack for system and user mode are the same.
+	 */
+
+	mrs r0,cpsr
+	bic r0,r0,#CPSR_MODE_MASK 	/* Clear the mode bits */
+	orr r0,r0,#SYS_MODE         /* Setup sys_mode */
+	orr r0,r0,#NoInt 			/* Disable irq,fiq interrupt */
+	msr cpsr,r0
+	ldr r13,usr_sys_stack_pointer
+
+	/* Set it back to SVC mode for further setup */
+	mrs r0,cpsr
+	bic r0,r0,#CPSR_MODE_MASK 	/* Clear the mode bits */
+	orr r0,r0,#SVC_MODE         /* Setup svc_mode */
+	orr r0,r0,#NoInt 			/* Disable irq,fiq interrupt */
+	msr cpsr,r0
+	
 	bl main
 
 	b start_os
 
 
+
 svc_stack_pointer: .word __svc_stack_top__
 irq_stack_pointer: .word __irq_stack_top__
-sys_stack_pointer: .word __sys_stack_top__
-usr_stack_pointer: .word __usr_stack_top__
+fiq_stack_pointer: .word __fiq_stack_top__
+usr_sys_stack_pointer: .word __usr_sys_stack_top__
 
 bss_start : .word __bss_start__
 bss_end : .word __bss_end__
